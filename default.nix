2b54ae4bc6a8a0db9965c458c77c6992020fe13c
Wittano <radoslaw.ratyna@gmail.com>
2023-12-20 15:35:01 +0100

    fix(nix): updated vendorHash in default.nix

--- default.nix
+++ default.nix
@@ -5,11 +5,9 @@ buildGoModule rec {
   src = ./.;
   version = "v1.0.0";
 
-  vendorHash = "sha256-plRphEIwtPoej+bM4fChhOjBGO/BJ2KoCjZnyD/Z634=";
+  vendorHash = "sha256-yGB8VC2gcsbfcASL6pPMEVeYNtWlpMQA981BGkevL/Q=";
 
   patches = [
     "./patches/fix(config)__changed_path_for_nix_build.patch"
   ];
-
-  goMod = ./.;
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/wittano/filebot
 
-go 1.20
+go 1.21
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0
