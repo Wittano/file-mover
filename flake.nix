b826c880689166165c83220180df062f2d1e5aeb
Wittano <radoslaw.ratyna@gmail.com>
2024-02-07 20:47:17 +0100

    feat(nix): added static nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,13 +1,18 @@
 {
-  description =
-    "Program to automaticlly moving files from one place to another";
+  description = "Program to automaticlly moving files from one place to another";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-23.11";
 
   outputs = { self, nixpkgs, }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       filebot = pkgs.callPackage ./default.nix { };
-    in {
+    in
+    {
       packages.x86_64-linux.default = filebot;
       nixosModules."filebot" = import ./service.nix;
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ go ];
+      };
     };
 }
