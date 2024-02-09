4a7364231b670d942debedfe389597f220041bc6
Wittano <radoslaw.ratyna@gmail.com>
2024-02-09 17:52:54 +0100

    fix(nix): update vendorHash

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ buildGoModule rec {
   src = ./.;
   version = "v1.0.0";
 
-  vendorHash = "sha256-yGB8VC2gcsbfcASL6pPMEVeYNtWlpMQA981BGkevL/Q=";
+  vendorHash = "sha256-zVQx3qaYqPlxirxX/FeuGHfKcsmvgWvLrKtQLkzVkgE=";
 
   patches = [
     "./patches/fix(config)__changed_path_for_nix_build.patch"
